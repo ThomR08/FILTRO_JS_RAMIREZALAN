a6278a74681ffe0a8bbef57d8528c487a72825e3
Alan Ramirez (Cl Cajasan) <thomasramirez0806@gmail.com>
2025-08-27 12:16:35 -0500

    feat :sparkles: Develop DOM, UI/UX and scripts

---
+++ scripts/modules.js
@@ -0,0 +1,25 @@
+export async function searchMovie(movieDiccionary) {
+    const apiResponse = await fetchMovies(movieDiccionary.movieName); // Buscar las peliculas
+    const moviesContain = document.querySelector('#movies'); // Inyectar al documento
+    moviesContain.innerHTML = "";
+    apiResponse.description.forEach(element => {
+        moviesContain.innerHTML+= `
+        <div class="card">
+            <img src="${element["#IMG_POSTER"]}" alt="">
+            <h2>${element["#TITLE"]}</h2>
+            <p>Year: ${element["#YEAR"]}</p>
+            <a href="${element["#IMDB_URL"]}"><h3>IMDB</h3></a>
+        </div>`
+    });
+}
+
+export async function fetchMovies(movieName) {
+    const response = await fetch(`https://imdb.iamidiotareyoutoo.com/search?q=${movieName}`, {
+        method: 'GET',
+        headers: {
+            'Accept': '*/*'
+        }
+    });
+    const data = await response.json();
+    return data;
+};
---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+import * as modules from './modules.js'
+
+document.addEventListener('DOMContentLoaded', async () => { // Esperar a que el DOM se cargue
+
+    document.querySelector('form').addEventListener('submit', async e =>{ // Añadir un evento para submit del formulario
+        e.preventDefault();
+        const movieDiccionary = Object.fromEntries(new FormData(e.target));  // Guardar la data del formulario en un JSON
+        console.log(movieDiccionary.movieName) // Obtener nombre de la pelicula
+        await modules.searchMovie(movieDiccionary)
+    });
+});
